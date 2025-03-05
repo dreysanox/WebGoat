4b82ec3d1a88289dc04517e4ad47121d3b12a146
Dreysanox <davidreymata@gmail.com>
2025-03-05 19:34:03 +0100

    Error de compilación supuestamente arreglado

---
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/SecureObjectInputStream.java
@@ -0,0 +1,33 @@
+package org.owasp.webgoat.lessons.deserialization;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SecureObjectInputStream extends ObjectInputStream {
+    private static final Set<String> allowedClasses = new HashSet<>();
+
+    static {
+        allowedClasses.add("org.dummy.insecure.framework.VulnerableTaskHolder");
+        allowedClasses.add("java.lang.String");
+        allowedClasses.add("java.time.LocalDateTime");
+        allowedClasses.add("java.time.LocalDate");
+        allowedClasses.add("java.time.LocalTime");
+    }
+
+    public SecureObjectInputStream(InputStream in) throws IOException {
+        super(in);
+    }
+
+    @Override
+    protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
+        String className = desc.getName();
+        if (!allowedClasses.contains(className)) {
+            throw new InvalidClassException("Clase no autorizada: " + className);
+        }
+        return super.resolveClass(desc);
+    }
+}
