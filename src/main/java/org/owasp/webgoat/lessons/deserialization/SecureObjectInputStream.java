1e95a45a487d544d02c73903a523c79f195e339e
Dreysanox <davidreymata@gmail.com>
2025-03-05 19:37:47 +0100

    2º try2

--- src/main/java/org/owasp/webgoat/lessons/deserialization/SecureObjectInputStream.java
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/SecureObjectInputStream.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
+import java.io.InvalidClassException;
 import java.util.HashSet;
 import java.util.Set;
 
